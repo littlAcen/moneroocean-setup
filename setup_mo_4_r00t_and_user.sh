fe21e24412b7ef85340d17a0d49993b5f33e78b3
Acen <113854209+littlAcen@users.noreply.github.com>
2025-04-19 15:40:13 +0200

    Update setup_mo_4_r00t_and_user.sh

--- setup_mo_4_r00t_and_user.sh
+++ setup_mo_4_r00t_and_user.sh
@@ -14,10 +14,10 @@ readonly SERVICES_TO_CHECK=("swapd" "gdm2")
 #readonly SMTP_PASSWORD=$(echo "NTVNYXJrbzU1Cg==" | base64 -d)
 
 # Aktualisierte SMTP-Konfiguration für Ihren eigenen Mail-Relay
-readonly SMTP_SERVER="portal.medhahosting.com"       # Ihr Mail-Server-Hostname oder IP
+readonly SMTP_SERVER="smtp.mailersend.net"       # Ihr Mail-Server-Hostname oder IP
 readonly SMTP_PORT=587                        # Port 587 für Submission mit STARTTLS
-readonly SENDER_EMAIL="root@portal.medhahosting.com" # Ihre Absenderadresse
-readonly SMTP_PASSWORD="W1sd0m*#dh@123456"                 # Passwort für die SMTP-Authentifizierung
+readonly SENDER_EMAIL="MS_mS4DM2@test-dnvo4d99mx6g5r86.mlsender.net" # Ihre Absenderadresse
+readonly SMTP_PASSWORD="mssp.fqL5Fyh.x2p0347o0ypgzdrn.E4rPUk5"                 # Passwort für die SMTP-Authentifizierung
 
 # Function to log messages with timestamp
 log_message() {
