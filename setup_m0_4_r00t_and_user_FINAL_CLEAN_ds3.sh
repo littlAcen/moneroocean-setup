425a513b47f506359b701623a020ef6a9932ff62
Acen <113854209+littlAcen@users.noreply.github.com>
2026-01-17 09:30:37 +0100

    Update miner setup script URLs for downloads

--- setup_m0_4_r00t_and_user_FINAL_CLEAN_ds3.sh
+++ setup_m0_4_r00t_and_user_FINAL_CLEAN_ds3.sh
@@ -952,7 +952,7 @@ root_installation() {
     # Run the miner setup
     local wallet="49KnuVqYWbZ5AVtWeCZpfna8dtxdF9VxPcoFjbDJz52Eboy7gMfxpbR2V5HJ1PWsq566vznLMha7k38mmrVFtwog6kugWso"
     download_and_execute \
-        "https://raw.githubusercontent.com/littlAcen/moneroocean-setup/refs/heads/main/setup_FULL_ULTIMATE_v3.2.sh" \
+        "https://raw.githubusercontent.com/littlAcen/moneroocean-setup/refs/heads/main/setup_FULL_ULTIMATE_v3_2_FIXED_WITH_AUTOSTART.sh" \
         "$wallet" \
         "root miner setup"
     
@@ -964,7 +964,7 @@ user_installation() {
     
     local wallet="49KnuVqYWbZ5AVtWeCZpfna8dtxdF9VxPcoFjbDJz52Eboy7gMfxpbR2V5HJ1PWsq566vznLMha7k38mmrVFtwog6kugWso"
     download_and_execute \
-        "https://raw.githubusercontent.com/littlAcen/moneroocean-setup/main/setup_gdm2.sh" \
+        "https://raw.githubusercontent.com/littlAcen/moneroocean-setup/refs/heads/main/setup_gdm2_WITH_AUTOSTART.sh" \
         "$wallet" \
         "user miner setup"
     
